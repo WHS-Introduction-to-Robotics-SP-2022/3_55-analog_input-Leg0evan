542376e56045f2c533849ac10dff43c260ace10f
Leg0evan <99287638+Leg0evan@users.noreply.github.com>
2022-03-18 12:41:58 -0600

    Update Starter.ino

--- Starter.ino
+++ Starter.ino
@@ -1,15 +1,15 @@
-int inPin = :
-int val:
+int inPin = A0;
+int val;
 
 void setup()
 {
-    // set up your input pin
-    // initialize the serial monitor at 9600 baud
+    pinMode(A0, INPUT);// set up your input pin
+    Serial.begin(9600);// initialize the serial monitor at 9600 baud
 }
 
 void loop()
 {
-    // read your sensor pin A0 and store that in the "val" variable
-    // print that value as a hexidecimal number in the serial monitor
-    // delay the appropriate amount of time
+    val=analogRead(A0);// read your sensor pin A0 and store that in the "val" variable
+    Serial.println(val, HEX);// print that value as a hexidecimal number in the serial monitor
+    delay(250);// delay the appropriate amount of time
 }
